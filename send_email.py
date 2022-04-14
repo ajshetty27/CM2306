75ad2b7de44d2f6943e640b18b9c3830f94e4e69
groomj <groomj@cardiff.ac.uk>
2022-04-14 19:37:59 +0100

    send email script

---
+++ send_email.py
@@ -0,0 +1,37 @@
+import os
+import base64
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
+
+file_path = '' #insert file path (include filetype in name)
+
+message = Mail(
+    from_email='groomj@cardiff.ac.uk',
+    to_emails='ShettyA6@cardiff.ac.uk',
+    subject='INTRUDER WTF',
+    html_content='<h1> Intruder Detected </h1><p>An intruder has been detected at your security camera. Please find attached the following image captured. </p>'
+)
+
+with open(file_path, 'rb') as f:
+    data = f.read()
+    f.close()
+encoded_file = base64.b64encode(data).decode()
+
+attachedFile = Attachment(
+    FileContent(encoded_file),
+    FileName(file_path),
+    FileType('application/jpg'),
+    Disposition('attachment')
+)
+
+message.attachment = attachedFile
+        
+try:
+    sg = SendGridAPIClient('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')
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.msg)
+
