5f36e2f3f69dca4973daffc0ecc0a6bf925cf698
Aryan Shetty <93198531+ajshetty27@users.noreply.github.com>
2022-04-25 16:32:41 +0100

    Checks if new people added to dropbox
    Checks if a new image has been added to dropbox and downloads that image to the local folder for facial recognition comparison

---
+++ add_people.py
@@ -0,0 +1,35 @@
+import dropbox
+import os
+
+dbx = dropbox.Dropbox('ACCESS TOKEN (will add when downloaded to raspberry pi)')
+
+dropbox_dict = {}
+local_list = []
+new_addition = []
+
+for entry in dbx.files_list_folder('').entries:
+    path = entry.name
+    path_check = entry.id
+    dropbox_dict[path] = path_check
+    #print(path)
+    #print(path_check)
+
+for filename in os.listdir("known_people/"):
+    #print(filename)
+    local_list.append(filename)
+
+print(dropbox_dict)
+print(local_list)
+
+#Checks new files in dropbox api
+new_addition = list(set(dropbox_dict.keys()) - set(local_list))
+print(new_addition)
+
+#Below function needs help with
+
+'''if not new_addition:
+    print("known_people is upto date")
+
+else:
+    for newfile in new_addition:
+        dbx.files_download_to_file('Download Path',dropbox_dict[newfile])'''
