b69ebb776a78b4711b535df4b5fbfbe3377ad709
groomj <groomj@cardiff.ac.uk>
2022-05-03 17:25:45 +0100

    send_sms functionality

---
+++ send_sms.py
@@ -0,0 +1,36 @@
+import os
+import http.client
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Sends SMS using infobip API
+
+
+BASE_URL = "jdzk4v.api.infobip.com"
+API_KEY = os.environ.get('INFOBIP_API_KEY')
+
+print('THE API KEY: ',API_KEY)
+
+SENDER = "InfoSMS"
+RECIPIENT = "447445086170"
+MESSAGE_TEXT = "ALERT - Intruder detected at your security camera. Check your email for more details."
+
+conn = http.client.HTTPSConnection(BASE_URL)
+
+payload1 = "{\"messages\":" \
+          "[{\"from\":\"" + SENDER + "\"" \
+          ",\"destinations\":" \
+          "[{\"to\":\"" + RECIPIENT + "\"}]," \
+          "\"text\":\"" + MESSAGE_TEXT + "\"}]}"
+
+headers = {
+    'Authorization': API_KEY,
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
+conn.request("POST", "/sms/2/text/advanced", payload1, headers)
+
+res = conn.getresponse()
+data = res.read()
+print(data.decode("utf-8"))
