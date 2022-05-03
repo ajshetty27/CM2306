bde7022da3056149fd846396dadd9cbd3015e884
Otto Hooper <votton.hooper@gmail.com>
2022-05-03 15:36:53 +0100

    Intruder button functionality

--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
 import os
-import webview
+import cv2
 
 
 def add_person():
@@ -10,12 +10,25 @@ def add_person():
 
 
 def intruder_images():
-    os.system('')
-
-def view_camera():
-    os.system('rpi_surveillance_camera.py')
-    webview.create_window('Camera View', 'https://www.google.com') #insert website here
-    webview.start()
+    # Open a new window
+    window = Toplevel()
+    window.title("Intruder Images")
+    window.geometry('500x500')
+    # Show the images in intruder_images folder
+    for i in os.listdir('intruder_images'):
+        # Open the image and resize it
+        img = Image.open('intruder_images/' + i)
+        img = img.resize((150, 150), Image.ANTIALIAS)
+        # Get the filename of the image
+        filename = os.path.splitext(i)[0]
+        # Convert the image to Tkinter format
+        img = ImageTk.PhotoImage(img)
+        # Create a label for the image and add it to the window, also add the filename
+        label = Label(window, image=img)
+        label.image = img
+        label.pack()
+        label = Label(window, text=filename)
+        label.pack()
 
 
 root = Tk()
@@ -41,11 +54,9 @@ separator.pack(fill='x')
 # Create buttons with commands
 btn = Button(root, text='Add Person', bd='4', command=add_person)
 btn2 = Button(root, text='Display Intruder Images', bd='4', command=intruder_images)
-btn3 = Button(root, text='View Camera Feed', bd='4', command=view_camera)
 
 # Set the position of buttons
 btn.place(relx=0.1, rely=0.80, relheight=0.1, relwidth=0.2)
 btn2.place(relx=0.4, rely=0.80, relheight=0.1, relwidth=0.2)
-btn3.place(relx=0.7, rely=0.80, relheight=0.1, relwidth=0.2)
 
 root.mainloop()
