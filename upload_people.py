ad0a7a028da97d439eb4d47d5d9db087e8b730c8
Otto Hooper <votton.hooper@gmail.com>
2022-05-06 16:22:09 +0100

    Check that files already exist

--- upload_people.py
+++ upload_people.py
@@ -4,9 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 dbx = dropbox.Dropbox(os.environ.get('DROPBOX_API_KEY'))
 
-# Upload people to  Dropbox
+# Upload people to but check if they already exist in Dropbox
 for filename in os.listdir("known_people/"):
-    print("Uploading " + filename)
-    dbx.files_upload(open("known_people/" + filename, 'rb').read(), '/' + filename)
-    print("Uploaded " + filename)
-    print("")
+    if filename.endswith(".jpg"):
+        with open("known_people/" + filename, "rb") as f:
+            try:
+                dbx.files_upload(f.read(), "/known_people/" + filename)
+            except dropbox.exceptions.AccessError:
+                print("File already exists in Dropbox")
+                continue
+            print("Uploaded " + filename)
