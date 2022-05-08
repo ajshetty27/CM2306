6e1c5ea77b9704a633b4ee08d3eec418feeed668
Aryan Shetty <93198531+ajshetty27@users.noreply.github.com>
2022-05-08 20:53:16 +0530

    Add comments to main.py

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import time
 import sys
 import grovepi
 
+#Detect ultrasonic sensor on port 3
 ultrasonic_ranger = 3
 
 while True:
     print("Working")
     try:
         distance = grovepi.ultrasonicRead(ultrasonic_ranger)
-        # change range here
+        #Range of detection of ultrasonic sensor 
         if distance < 30:
             print("Detected: Wait like 20sec")
+            #If face is detected run facial recognition
             exec(open("facial_recognition.py").read())
             time.sleep(300)
         else:
