6520c9b6c134c6de547b550127dac06313e6fcfb
Restyled.io <commits@restyled.io>
2022-07-11 18:41:53 +0000

    Restyled by black

--- menu.py
+++ menu.py
@@ -10,18 +10,20 @@ def sync_people():
     exec(open("add_people.py").read())
     # Provide a message box to confirm that the person has been added
     messagebox.showinfo(
-        "Sync complete", "Latest known people have been added to the local known_people folder")
+        "Sync complete",
+        "Latest known people have been added to the local known_people folder",
+    )
 
 
 def intruder_images():
     # Open a new window
     window = Toplevel()
     window.title("Intruder Images")
-    window.geometry('500x500')
+    window.geometry("500x500")
     # Show the images in intruder_images folder
-    for i in os.listdir('intruder_images'):
+    for i in os.listdir("intruder_images"):
         # Open the image and resize it
-        img = Image.open('intruder_images/' + i)
+        img = Image.open("intruder_images/" + i)
         img = img.resize((150, 150), Image.ANTIALIAS)
         # Get the filename of the image
         filename = os.path.splitext(i)[0]
@@ -39,11 +41,11 @@ def display_known_people():
     # Open a new window
     window = Toplevel()
     window.title("Known People")
-    window.geometry('500x500')
+    window.geometry("500x500")
     # Show the images in known_people folder
-    for i in os.listdir('known_people'):
+    for i in os.listdir("known_people"):
         # Open the image and resize it
-        img = Image.open('known_people/' + i)
+        img = Image.open("known_people/" + i)
         img = img.resize((150, 150), Image.ANTIALIAS)
         # Get the filename of the image
         filename = os.path.splitext(i)[0]
@@ -56,6 +58,7 @@ def display_known_people():
         label = Label(window, text=filename)
         label.pack()
 
+
 # Get your own IP address
 
 
@@ -63,13 +66,14 @@ def get_own_ip():
     ip_address = socket.gethostbyname(socket.gethostname())
     return ip_address
 
+
 # Open a new window and display the camera feed
 
 
 def view_camera():
-    os.system('python3 rpi_camera_surveillance_system.py')
+    os.system("python3 rpi_camera_surveillance_system.py")
     # Open camera in your browser with your IP address
-    webbrowser.open(get_own_ip() + ':8080')
+    webbrowser.open(get_own_ip() + ":8080")
 
 
 root = Tk()
@@ -79,26 +83,23 @@ root.geometry("800x650")
 root.title("Communication Networks")
 # root.configure(bg='white smoke')
 
-w = Label(root, text='Communication Networks',
-          font=("Helvetica", "30", 'bold'))
+w = Label(root, text="Communication Networks", font=("Helvetica", "30", "bold"))
 w.pack(pady=20)
 
-separator = ttk.Separator(root, orient='horizontal')
-separator.pack(fill='x')
+separator = ttk.Separator(root, orient="horizontal")
+separator.pack(fill="x")
 
 img = ImageTk.PhotoImage(Image.open("example.png"))
 panel = Label(root, image=img)
 panel.pack(side="top", fill="both")
 
-separator = ttk.Separator(root, orient='horizontal')
-separator.pack(fill='x')
+separator = ttk.Separator(root, orient="horizontal")
+separator.pack(fill="x")
 
 # Create buttons with commands
-btn = Button(root, text='Add Person', bd='4', command=sync_people)
-btn2 = Button(root, text='Display Intruder Images',
-              bd='4', command=intruder_images)
-btn3 = Button(root, text='Display Known People',
-              bd='4', command=display_known_people)
+btn = Button(root, text="Add Person", bd="4", command=sync_people)
+btn2 = Button(root, text="Display Intruder Images", bd="4", command=intruder_images)
+btn3 = Button(root, text="Display Known People", bd="4", command=display_known_people)
 
 # Set the position of buttons
 btn.place(relx=0.1, rely=0.80, relheight=0.1, relwidth=0.2)
